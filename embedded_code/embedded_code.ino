2921b997155faa77a56d36a90a6ca8ed3ce1b18f
Leonardo Benitez <lsbenitezpereira@gmail.com>
2022-05-23 23:24:38 -0300

    basic timer usage

--- embedded_code/embedded_code.ino
+++ embedded_code/embedded_code.ino
@@ -1,17 +1,33 @@
+#include <Ticker.h>
 
-// the setup function runs once when you press reset or power the board
+volatile byte state = LOW;
+const float sample_rate_ms = 0.4666;
+int i = 0; // iteration counter
+
+void control_equation(){
+  state = !state;
+  digitalWrite(LED_BUILTIN, state);
+  //Serial.println("> eq");
+  i++;
+  timer1_write(sample_rate_ms*5000);
+}
+
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+
+  /*******************/
+  /* Configure Timer */ 
+  timer1_attachInterrupt(control_equation);
+  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
+  timer1_write(sample_rate_ms*5000);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
-  Serial.println(".");
-  Serial.println("> eq");
+  Serial.println(i);
+  i = 0;
+  
 }
